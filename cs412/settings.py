698afd0b22ccb6d393fff04f313e3cdb7d9d0041
Abdul Rafay <rafaya@bu.edu>
2026-01-22 12:42:11 -0500

    Added all files for deployment." git commit -m "added all files for deployment." clear :qwe

--- cs412/settings.py
+++ cs412/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-v*6fsau=-r5+zik$utp&2w+lx%d#ck(&7t-5@@#$8mx*fydt^_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [cs-webapps.bu.edu]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cs-webapps.bu.edu']
 
 
 # Application definition
