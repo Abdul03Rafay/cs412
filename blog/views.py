1c02f462838840036577cc298f3c50b0b50efbc9
Abdul Rafay <rafaya@bu.edu>
2026-02-10 12:33:30 -0500

    "Started module 3 example; blog."

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Article
+from django.views.generic import ListView
+
+# Create your views here.
+
+class ShowAllView(ListView):
+    '''Create a subclass of ListView to display all blog articles.'''
+    
+    model = Article
+    template_name = 'blog/show_all.html'
+    context_object_name = 'articles'
